e3fccbe9bfea6860577fda6e13a6da5dbff8c4f5
guillenjoshua <guillenjoshua@gmail.com>
2020-07-27 17:15:31 -0500

    Generate Password Working-Code Complete

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
+// Assignment Code
+
+var lengthEl = document.querySelector("#length")
+var uppercaseEl = document.querySelector("#uppercase")
+var numbersEl = document.querySelector("#numbers")
+var symbolEl = document.querySelector("#special")
+
 var generateBtn = document.querySelector("#generate");
 
 
@@ -7,43 +14,34 @@ var numb = "123456789"
 var symbol = "!@#$%^&"
 
 
-var lengthEl = document.querySelector("#length")
-var uppercaseEl = document.querySelector("#uppercase")
-var numbersEl = document.querySelector("#numbers")
-var symbolEl = document.querySelector("#special")
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var choices = lower;
+    (uppercaseEl.checked) ? choices += upper : '';
+    (numbersEl.checked) ? choices += numb : '';
+    (symbolEl.checked) ? choices += symbol : '';
+
+  passwordText.value = generatePassword(lengthEl.value, choices)
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
 
-    // var choices = lower;
-    // (uppercaseEl.checked) ? choices += upper : '';
-    // (numbersEl.checked) ? choices += numb : '';
-    // (symbolEl.checked) ? choices += symbolEl : '';
-
-
-  // console.log(generatePassword(12,upper))
+   
 
 // Functiion Generate Password and stuff
+
 function generatePassword (x,howMany){  
-//alert('hello')
 
 var password = document.querySelector("#password");
 var passBox = "";
 for (var i = 0; i < x; i++){
-  // var chars = "abcdefghijklmnopqrstuvwxyz123456789+-*/"
   var random = Math.floor(Math.random()*howMany.length);
   passBox += howMany.charAt(random)
 }
   return passBox;
 }
-//lengthEl.value, choices
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 03-javascript-passwordGenerator
+
+
+A few things to note for this homework.  
+
+I opted not to use prompts. After reading through the Eloquent Javascript section on console.log functions, pg 26, it states that the prompt function is not used much in modern programming and also, our TA said it was ok to use checkboxes or input fields. So I chose checkboxes. 
+
+I used ternary functions in the function writePassword, starting line 18, because it seemed easier and I was having a hard time or getting confused with my if statements. 
+
+The writePassword parameter in the addEventListener, line 32, kept throwing me off and wasnt able to get my password to recognize the length of the password, checkboxes and generate a password.  Realized I needed to change password in line 27 to generatePassword. 
+
+The function writePassword section, starting line 18, in particular confused me, mostly line 20 becuase it had query.selector inside the function when I kept thinking it should be part of the assignment code section at the top.
+
+I did leave lowercase as a non-option purposefully, mainly since I've never seen or had a password generated without lowercase letters.  Otherwise, I would have created a lowercaseEl variable, a checkbox in HTML and made lower a ternary function on line 22.  
+
+Im not sure if we were supposed to use Math in this homework, but everything I was reading directed towards using Math in order to generate the length of number of characters chosen for the password.  
